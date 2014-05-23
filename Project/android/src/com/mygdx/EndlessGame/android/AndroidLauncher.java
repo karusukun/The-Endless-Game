ee4c532dd8d2b1f880fca1f8cfaa460b6f298f27
karusukun <karusukun@gmail.com>
2014-05-22 19:29:41 -0600

    project structure Libgdx
    creacion de la estructura del proyecto con gradle

---
+++ Project/android/src/com/mygdx/EndlessGame/android/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.mygdx.EndlessGame.android;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.mygdx.EndlessGame.appMain;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new appMain(), config);
+	}
+}
