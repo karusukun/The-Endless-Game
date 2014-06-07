0af8b6434529a56f74152203baaf90e2a1ec6d37
nife0222 <nife0820@gmail.com>
2014-06-06 21:15:30 -0700

    cleanup 3

--- Project/core/src/logic/GraphAlgorithms.java
+++ Project/core/src/logic/GraphAlgorithms.java
@@ -63,6 +63,7 @@ public class GraphAlgorithms {
 
             return false;
         }catch(Exception e){
+            e.printStackTrace();
             System.out.println(e.toString());
             return false;
         }
